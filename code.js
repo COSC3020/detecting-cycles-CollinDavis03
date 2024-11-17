93ae8cf112d50d8094715473c60676f6af5b8e37
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-11-17 09:13:16 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
 function hasCycle(graph) {
-    return false;
+    //Track the visited nodes
+    const visited = new Set(); 
+    //DFS = Depth First Search 
+    function DFS(node, parent) { 
+        visited.add(node); //Mark current node visited
+
+        //Iterate over all neighbors of current node
+        for (const neighbor of graph[node]) {
+            if (!visited.has(neighbor)) {
+                if (DFS(neighbor, node)) return true; //Recur on unvisited neighbors
+            } else if (neighbor !== parent) {
+                return true; //cycle detected 
+            }
+        }
+        return false; //No cycle detected at all
+    }
+    //Handle disconnected components 
+    for (const node in graph) {
+        if (!visited.has(Number(node))) {
+            if (DFS(Number(node), - 1)) return true; //Start DFS if node has not been visited
+        }
+    }
+    return false; //No cycle detected. 
 }
